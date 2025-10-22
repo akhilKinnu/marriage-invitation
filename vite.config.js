7b8989d09c87309be8ab2c7c480fb1196f7beb53
Kondapalkala Nikhil <nikhilk@Mac.lan>
2025-10-22 13:45:16 +0530

    xD

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/marriage-invitation/',
     plugins: [react()],
     resolve: {
         alias: {
